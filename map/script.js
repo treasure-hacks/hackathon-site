4e4dab692efe793ac79f33cb3278362a1bd743dd
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2021-09-25 07:21:23 -0700

    change Zoom Levels and API Endpoint

--- map/script.js
+++ map/script.js
@@ -1,15 +1,14 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic292aW5pbjQ4IiwiYSI6ImNrdHp0MG83eDNncDMycG8zdTk4bzNtMTkifQ.29HdXDPxiJKo5xU4-B1edw'
 const map = new mapboxgl.Map({
   container: 'map',
-  minZoom: window.innerHeight / 750,
-  zoom: window.innerHeight / 650,
+  zoom: window.innerHeight / 350 - 1,
   style: 'mapbox://styles/mapbox/dark-v10'
 })
 map.addControl(new mapboxgl.NavigationControl());
 
 // Fetch the location markers for the map
 const xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://localhost:5600/forms/location-markers')
+xhr.open('GET', 'http://api.treasurehacks.dev/forms/location-markers')
 xhr.onreadystatechange = function() {
   if (xhr.readyState !== 4) return
   if (xhr.status !== 200) return console.error(new Error('Unable to load data'))
