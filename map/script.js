67a352d5526a573fbb66f5c26ec98af76eab1dd5
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2021-09-25 07:11:45 -0700

    remove comments

--- map/script.js
+++ map/script.js
@@ -15,11 +15,6 @@ xhr.onreadystatechange = function() {
   if (xhr.status !== 200) return console.error(new Error('Unable to load data'))
   const markers = JSON.parse(xhr.responseText)
   markers.items.forEach(m => {
-    // const svg = document.createElement('svg')
-    // svg.style.width = svg.style.height = '100px'
-    // svg.setAttribute('width', 100)
-    // svg.setAttribute('height', 100)
-    // svg.innerHTML = `<circle cx="50" cy="50" r="5" fill="red" /><circle cx="50" cy="50" r="5" fill="red" class="ring" />`
     const container = document.createElement('div')
     container.classList.add('marker-container')
     container.title = m.location || 'Approximate Participant School Location'
@@ -28,31 +23,6 @@ xhr.onreadystatechange = function() {
     img.src = 'marker.png'
     container.appendChild(img)
     new mapboxgl.Marker(container).setLngLat([ m.lng, m.lat]).addTo(map);
-    // new google.maps.Marker({
-    //   position: { lat: m.lat, lng: m.lng },
-    //   map,
-    //   title: "Approximate Participant School Location",
-    //   icon: {
-    //     path: 'M5 10C7.76142 10 10 7.76142 10 5C10 2.23858 7.76142 0 5 0C2.23858 0 0 2.23858 0 5C0 7.76142 2.23858 10 5 10Z',
-    //     fillColor: "transparent",
-    //     fillOpacity: 0.92,
-    //     strokeWeight: 0,
-    //     rotation: 0,
-    //     scale: 1,
-    //     anchor: new google.maps.Point(5, 5)
-    //   }
-    // });
   })
 }
 xhr.send()
-
-// function initMap() {
-//   const myLatLng = { lat: 0, lng: 0 };
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     minZoom: window.innerHeight / 400,
-//     zoom: window.innerHeight / 383,
-//     center: myLatLng,
-//     // mapId: '86ca340ae117e983'
-//   });
-// }
-
