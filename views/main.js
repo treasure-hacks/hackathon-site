cac779faa3359b808ee929d2a18b83cd5854a76f
Ethan <59261562+ethank2222@users.noreply.github.com>
2021-10-12 17:29:13 -0700

    final push

--- views/main.js
+++ views/main.js
@@ -95,6 +95,7 @@ function categoryDetails(target){
 		case "best-overall": {
 			resetAll();
 			target.style.borderBottom = "3px solid black";
+			target.style.paddingBottom = "10px";
 			document.getElementById("category-title").innerHTML = "Best Overall";
 			document.getElementById("category-description").innerHTML = "The \"Best Solo Hack\" award will be given to a single hacker with the best solo project in the entire Hackathon.";
 			document.getElementById("category-prizes").innerHTML = "Prizes for this category include 1 $30 Amazon Gift Card For Each Group Member, 1 Cloud Stinger Headsets For Each Group Member, and 1 Alloy FPS RGB Refurb Keyboard For Each Group Member.";
@@ -106,6 +107,7 @@ function categoryDetails(target){
 			resetAll();
 			target.style.borderBottom = "3px solid black";
 			document.getElementById("category-title").innerHTML = "Best Solo Hack";
+			target.style.paddingBottom = "10px";
 			document.getElementById("category-description").innerHTML = "The \"Best Overall\" award will be given to a hacker or team who best demonstrates their project&rsquo;s creativity, practicality, engineering excellence, and presentation.";
 			document.getElementById("category-prizes").innerHTML = "Prizes for this category include 1 $30 Amazon Gift Card, 1 Cloud Stinger Headset, 1 Alloy FPS RGB Refurb Keyboard, and 1 Goliath Automation and Robotics Monster Kit.";
 			break;
@@ -114,6 +116,7 @@ function categoryDetails(target){
 			resetAll();
 			target.style.borderBottom = "3px solid black";
 			document.getElementById("category-title").innerHTML = "Best Duo Hack";
+			target.style.paddingBottom = "10px";
 			document.getElementById("category-description").innerHTML = "Collaboration skills are super important in the programming world! The \"Best Duo Hack\" award will be given to a team of two hackers with the best project.";
 			document.getElementById("category-prizes").innerHTML = "Prizes for this category include 1 $30 Amazon Gift Card Each, 1 Cloud Stinger Headset Each, and 1 Alloy FPS RGB Refurb Keyboard Each.";
 			break;
@@ -122,6 +125,7 @@ function categoryDetails(target){
 			resetAll();
 			target.style.borderBottom = "3px solid black";
 			document.getElementById("category-title").innerHTML = "Most Aesthetically Pleasing";
+			target.style.paddingBottom = "10px";
 			document.getElementById("category-description").innerHTML = "Are you a creative and artistic person? The \"Most Aesthetically Pleasing\" award will be given to the project with the most polish, best presentation, and most intuitive user experience.";
 			document.getElementById("category-prizes").innerHTML = "The prize for this category is 1 $30 Amazon Gift Card.";
 			break;
@@ -130,6 +134,7 @@ function categoryDetails(target){
 			resetAll();
 			target.style.borderBottom = "3px solid black";
 			document.getElementById("category-title").innerHTML = "Most People Helped";
+			target.style.paddingBottom = "10px";
 			document.getElementById("category-description").innerHTML = "At Treasure Hacks, we value teamwork and working together. The \"Most People Helped\" award will be given to the hacker who has helped the most other groups complete their projects.";
 			document.getElementById("category-prizes").innerHTML = "Prizes for this category include 1 $30 Amazon Gift Card Each, 1 Cloud Stinger Headset Each, and 1 Alloy FPS RGB Refurb Keyboard Each.";
 			break;
@@ -138,6 +143,7 @@ function categoryDetails(target){
 			resetAll();
 			target.style.borderBottom = "3px solid black";
 			document.getElementById("category-title").innerHTML = "Best Qoom Hack";
+			target.style.paddingBottom = "10px";
 			document.getElementById("category-description").innerHTML = "Qoom is a dream tool built to bring anything you imagine into reality with seamless development tools to create projects with HTML, CSS, and Javascript. This award will be given to the best project using Qoom.";
 			document.getElementById("category-prizes").innerHTML = "Prizes for this category include a 1-Year Pro Plan Package, a feature on Qoom, and an invitation to the Qoom Creator Group";
 			break;
@@ -146,6 +152,7 @@ function categoryDetails(target){
 			resetAll();
 			target.style.borderBottom = "3px solid black";
 			document.getElementById("category-title").innerHTML = "Best Sashido Hack";
+			target.style.paddingBottom = "10px";
 			document.getElementById("category-description").innerHTML = "SashiDo is offering a special prize for the most original Image project build with SashiDo & Teachable Machine by Google.";
 			document.getElementById("category-prizes").innerHTML = "Prizes for this category include a short blog post on SashiDo social media, a possible internship, and a special prize from Sashido.";
 			break;
@@ -154,6 +161,7 @@ function categoryDetails(target){
 			resetAll();
 			target.style.filter = "invert(63%) sepia(57%) saturate(518%) hue-rotate(9deg) brightness(100%) contrast(89%);"
 			target.style.borderBottom = "3px solid black";
+			target.style.paddingBottom = "10px";
 			document.getElementById("category-title").innerHTML = "All Participants";
 			document.getElementById("category-description").innerHTML = "We also offer prizes for all of our participants!";
 			document.getElementById("category-prizes").innerHTML = "Each participant will receive a Treasure Hacks sticker, a Hack club Sticker, an Echo3D business plan, a Replit subscription, $120 Sashido credits, an InterviewCake subscription, a Qoom subscription, and a Treasure Hacks T-shrit.";
@@ -165,5 +173,6 @@ function resetAll(){
 	var list = ["best-overall","best-solo","best-duo","most-aesthetically-pleasing","most-people-helped","qoom","sashido", "all-participants"];
 	for (var i = 0; i < list.length; i++){
 		document.getElementById(list[i]).style.borderColor = "rgba(0,0,0,0)";
+		target.style.paddingBottom = "13px";
 	}
 }
