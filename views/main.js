996ee9c532d94e7d3fc140f4b0d49cfd334636c0
Ethan <59261562+ethank2222@users.noreply.github.com>
2021-10-12 17:25:26 -0700

    Update main.js

--- views/main.js
+++ views/main.js
@@ -95,7 +95,6 @@ function categoryDetails(target){
 		case "best-overall": {
 			resetAll();
 			target.style.borderBottom = "3px solid black";
-			target.style.paddingBottom = "10px";
 			document.getElementById("category-title").innerHTML = "Best Overall";
 			document.getElementById("category-description").innerHTML = "The \"Best Solo Hack\" award will be given to a single hacker with the best solo project in the entire Hackathon.";
 			document.getElementById("category-prizes").innerHTML = "Prizes for this category include 1 $30 Amazon Gift Card For Each Group Member, 1 Cloud Stinger Headsets For Each Group Member, and 1 Alloy FPS RGB Refurb Keyboard For Each Group Member.";
