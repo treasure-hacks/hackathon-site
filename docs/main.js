93f061daef54c6751f705a934837501a78866a3e
Awesome-E <awesome33e@gmail.com>
2021-10-14 14:37:40 -0700

    add mobile navbar content into same HBS file
    animations are a bonus

--- docs/main.js
+++ docs/main.js
@@ -37,47 +37,15 @@ for (i = 0; i < acc.length; i++) {
 let toggle = document.getElementById("hamburger");
 
 toggle.addEventListener("click", function () {
-  if (toggle.className === "fa fa-bars hamburger fa-lg"){
-    toggle.className = "fa fa-times hamburger fa-lg";
-    var smenu = document.createElement("nav");
-    smenu.id = "menu-on-small";
-    document.body.appendChild(smenu);
-    document.getElementById("navigation").style.boxShadow = "none";
-    var x;
-	
-	
-    x = smenu.appendChild(document.createElement("a"));
-    x.href = "/schedule.html";
-    x.innerHTML = "Schedule";
-    x.className = "menu-item"
-
-    x = smenu.appendChild(document.createElement("a"));
-    x.href = "/team.html";
-    x.innerHTML = "Team";
-    x.className = "menu-item"
-
-    x = smenu.appendChild(document.createElement("a"));
-    x.href = "/sponsorship-information.pdf";
-    x.innerHTML = "Sponsor Us";
-    x.className = "menu-item"
-
-    x = smenu.appendChild(document.createElement("a"));
-    x.href = "https://docs.google.com/forms/d/e/1FAIpQLSfd1cNojHTgsXazii8Q-WoecmSdOxn94rCEJb_q_A6R29Ribw/viewform";
-    x.innerHTML = "Register";
-    x.className = "menu-item"
-
-    x = smenu.appendChild(document.createElement("a"));
-    x.href = "https://bank.hackclub.com/donations/start/treasure-hacks";
-    x.innerHTML = "Donate";
-    x.className = "menu-item"
-	
-	
-	
-  }
-  else{
-    document.getElementById("menu-on-small").remove();
-    toggle.className = "fa fa-bars hamburger fa-lg";
-    document.getElementById("navigation").style.boxShadow = "0px 0px 8px 0px rgb(0 0 0)";
+  const nav = document.getElementById("navigation")
+  if (nav.classList.contains('open')) {
+    nav.parentNode.classList.remove('open')
+    toggle.className = 'fa fa-bars hamburger fa-lg'
+    document.getElementById('navigation').classList.remove('open')
+  } else {
+    nav.parentNode.classList.add('open')
+    document.getElementById('navigation').classList.add('open')
+    toggle.className = 'fa fa-close hamburger fa-lg'
   }
 });
 
