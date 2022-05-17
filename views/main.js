63144d2eae9fca9d16a4b3087d52634d3b83fe6d
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2022-05-17 11:19:08 -0700

    fix schedule path & page

--- views/main.js
+++ views/main.js
@@ -1,21 +1,3 @@
-const countDownDate = new Date('Mar 25, 2022 17:00:00').getTime()
-if (window.location.pathname == '/schedule.html') {
-  var y = setInterval(function () {
-    const now = new Date().getTime()
-    const distance = countDownDate - now
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000)
-    document.getElementById('countdown').innerHTML = days + 'd ' + hours + 'h ' +
-      minutes + 'm ' + seconds + 's '
-    if (distance < 0) {
-      clearInterval(y)
-      document.getElementById('countdown').innerHTML = 'In Progress!'
-    }
-  }, 1000)
-}
-
 const acc = document.getElementsByClassName('accordion')
 let i
 for (i = 0; i < acc.length; i++) {
