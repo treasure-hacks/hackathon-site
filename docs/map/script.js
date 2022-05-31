7b8ff629f4083132d7718b6a177f15244dedfe02
Awesome_E <54484616+Awesome-E@users.noreply.github.com>
2022-05-30 19:43:15 -0700

    Make map require interaction to show
    change made to improve page performance

--- docs/map/script.js
+++ docs/map/script.js
@@ -1,28 +1,35 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic292aW5pbjQ4IiwiYSI6ImNrdHp0MG83eDNncDMycG8zdTk4bzNtMTkifQ.29HdXDPxiJKo5xU4-B1edw'
-const map = new mapboxgl.Map({
-  container: 'map',
-  zoom: window.innerHeight / 350 - 1,
-  style: 'mapbox://styles/mapbox/dark-v10'
-})
-map.addControl(new mapboxgl.NavigationControl())
-
-// Fetch the location markers for the map
-const xhr = new XMLHttpRequest()
-xhr.open('GET', 'https://api.treasurehacks.dev/forms/location-markers')
-xhr.onreadystatechange = function () {
-  if (xhr.readyState !== 4) return
-  if (xhr.status !== 200) return console.error(new Error('Unable to load data'))
-  const markers = JSON.parse(xhr.responseText)
-  document.querySelector('.participant-count').innerText = markers.count + ' Total Participants'
-  markers.items.forEach(m => {
-    const container = document.createElement('div')
-    container.classList.add('marker-container')
-    container.title = m.location || 'Approximate Participant School Location'
-    const img = document.createElement('img')
-    img.width = img.width = 48
-    img.src = 'marker.png'
-    container.appendChild(img)
-    new mapboxgl.Marker(container).setLngLat([m.lng, m.lat]).addTo(map)
+function renderMap () {
+  document.body.removeEventListener('click', renderMap)
+  const map = new mapboxgl.Map({
+    container: 'map',
+    zoom: window.innerHeight / 350 - 1,
+    style: 'mapbox://styles/mapbox/dark-v10'
   })
+  map.addControl(new mapboxgl.NavigationControl())
+
+  // Fetch the location markers for the map
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', 'https://api.treasurehacks.dev/forms/location-markers')
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) return
+    if (xhr.status !== 200) return console.error(new Error('Unable to load data'))
+    const markers = JSON.parse(xhr.responseText)
+    document.querySelector('.participant-count').innerText = markers.count + ' Total Participants'
+    markers.items.forEach(m => {
+      const container = document.createElement('div')
+      container.classList.add('marker-container')
+      container.title = m.location || 'Approximate Participant School Location'
+      const img = document.createElement('img')
+      img.width = img.width = 48
+      img.src = 'marker.png'
+      container.appendChild(img)
+      new mapboxgl.Marker(container).setLngLat([m.lng, m.lat]).addTo(map)
+    })
+  }
+  xhr.send()
+
+  document.querySelector('.map-container .participant-count').removeAttribute('style')
+  document.querySelector('.cover').remove()
 }
-xhr.send()
+document.body.addEventListener('click', renderMap)
