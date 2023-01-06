eb632f6396dff18ccc0a8b5f730741583687e47c
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2023-01-06 09:30:06 -0800

    Update categories and prizes

--- views/script.js
+++ views/script.js
@@ -8,7 +8,18 @@ splide = new Splide('.mountain-slider', {
   flickMaxPages: 0.1
 })
 
+const categorySplide = new Splide('.category-box-container', {
+  type: 'loop',
+  focus: 'center',
+  drag: true,
+  autoWidth: true,
+  gap: '16px',
+  trimSpace: false,
+  updateOnMove: true
+})
+
 window.addEventListener('load', () => {
   splide.mount(window.splide.Extensions)
   splide.root.classList.add('loaded')
+  categorySplide.mount(window.splide.Extensions)
 })
