8d59b0834aad53ae5f3df0178d2813fae689ed7b
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2023-02-28 14:58:42 -0800

    clean up localStorage code

--- views/register/script.js
+++ views/register/script.js
@@ -1,43 +1,13 @@
 const form = document.querySelector('form')
-// function test () {
-//   const data = [...new FormData(form).entries()].map(([inputName, value]) => {
-//     const element = document.querySelector(`[name="${inputName}"]:not([type=radio]:not(:checked))`)
-//     const type = element.dataset.type || element.type
-//     const name = element.dataset.for || inputName
-//     return { name, value, element, type }
-//   }).filter(({ value, type }) => (type !== 'file' && value) || value.size)
-//   return data.reduce((dict, next) => {
-//     switch (next.type) {
-//       case 'checkbox': {
-//         const arr = dict[next.name] || []
-//         arr.push(next.value)
-//         dict[next.name] = arr
-//         break
-//       }
-//       case 'number': dict[next.name] = Number(next.value); break
-//       case 'boolean': dict[next.name] = !!Number(next.value); break
-//       default: dict[next.name] = next.value
-//     }
-//     return dict
-//   }, {})
-// }
-
-// form.addEventListener('submit', e => {
-//   // Check validity of form; prevent submission if client says invalid.
-//   // Server will still validate, of course
-
-//   console.log(test())
-//   // e.preventDefault()
-// })
 
 function save () {
   const data = new FormData(form)
   const entries = [...(new URLSearchParams(data)).entries()]
-  localStorage.FORMTHING = JSON.stringify(entries)
+  localStorage.savedRegistration = JSON.stringify(entries)
 }
 function load () {
   updateConditionalShows() // Hide at first so that if it returns, it's still updated
-  const entries = JSON.parse(localStorage.FORMTHING || '0')
+  const entries = JSON.parse(localStorage.savedRegistration || '0')
   if (!entries) return
   for (const [key, val] of entries) {
     const input = form.elements[key]
