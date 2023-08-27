b491cb91875f158cd924a8a099b6b954a11c77cd
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2023-08-27 11:54:46 -0700

    make registration self-contained
    this is to make it easier to copy and paste it to other Treasure Hacks event pages

--- views/register/script.js
+++ views/register/script.js
@@ -29,6 +29,27 @@ function load () {
   updateConditionalShows()
 }
 
+/**
+ * Returns an item from or an entire array of ancestor elements
+ * @param {HTMLElement} element The element to obtain the tree from
+ * @param {number} index The number of nodes to travel up (or down) the tree
+ * @param {boolean} topDown Whether to return a top-down tree that starts with
+ * the document element and ends with the specified element
+ * @returns {HTMLElement | HTMLElement[]} The array if no index is specified, or the node at the
+ * specified index of the node tree
+ */
+function nodeTree (element, index, topDown) {
+  const tree = []
+  let target = element
+  while (target && target !== document.documentElement) {
+    tree.push(target)
+    target = target.parentNode
+  }
+  if (topDown) tree.reverse()
+  if (index == null) return tree
+  return tree[index] || null
+}
+
 const currentURL = new URL(location.href)
 document.getElementById('referrer-input').value = currentURL.origin + currentURL.pathname
 
